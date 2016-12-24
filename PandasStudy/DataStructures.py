589667d77c4d08361690ece776810518a8ba6387
GaobinWang <gaobin513@qq.com>
2016-12-24 14:47:47 +0800

    pandas数据结构

---
+++ PandasStudy/DataStructures.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# --*-- coding:utf-8 --*--
+"""
+本篇主要介绍pandas库中的数据结构。
+参考资料：
+http://pandas.pydata.org/pandas-docs/stable/dsintro.html#dsintro
+"""
+
+import pandas as pd
+import numpy as np
+
+#pandas中的数据结构一共有三种：Series DataFrame Panel
+###Series
+
+###Pandas
+
+
+###Panel
+
+
