d44508d1c3b2389d63376a2275fcac5d1e6f9e44
GaobinWang <gaobin513@qq.com>
2019-06-07 12:43:48 +0800

    pdf

---
+++ PythonPlayWithPDF.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 11:41:37 2019
+
+@author: Lesile
+"""
+#%%调用常见模块
+import os
+import pandas as pd
+import numpy as np
+from PyPDF2 import PdfFileReader
+from PyPDF2 import PDFMiner
+
+path = "E:\Github\AdvancedPython"
+os.chdir(path)
+
+#%%pypdf2处理中文文本
+import sys
+import importlib
+importlib.reload(sys)
+
+from pdfminer.pdfparser import PDFParser,PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LTTextBoxHorizontal,LAParams
+from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
+
+'''
+ 解析pdf 文本，保存到txt文件中
+'''
+path = r'testdata.pdf'
+def parse():
+    fp = open(path, 'rb') # 以二进制读模式打开
+    #用文件对象来创建一个pdf文档分析器
+    praser = PDFParser(fp)
+    # 创建一个PDF文档
+    doc = PDFDocument()
+    # 连接分析器 与文档对象
+    praser.set_document(doc)
+    doc.set_parser(praser)
+
+    # 提供初始化密码
+    # 如果没有密码 就创建一个空的字符串
+    doc.initialize()
+
+    # 检测文档是否提供txt转换，不提供就忽略
+    if not doc.is_extractable:
+        raise PDFTextExtractionNotAllowed
+    else:
+        # 创建PDf 资源管理器 来管理共享资源
+        rsrcmgr = PDFResourceManager()
+        # 创建一个PDF设备对象
+        laparams = LAParams()
+        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+        # 创建一个PDF解释器对象
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+        # 循环遍历列表，每次处理一个page的内容
+        for page in doc.get_pages(): # doc.get_pages() 获取page列表
+            interpreter.process_page(page)
+            # 接受该页面的LTPage对象
+            layout = device.get_result()
+            # 这里layout是一个LTPage对象 里面存放着 这个page解析出的各种对象 一般包括LTTextBox, LTFigure, LTImage, LTTextBoxHorizontal 等等 想要获取文本就获得对象的text属性，
+            for x in layout:
+                if (isinstance(x, LTTextBoxHorizontal)):
+                    with open(r'1.txt', 'a') as f:
+                        results = x.get_text()
+                        print(results)
+                        f.write(results + '\n')
+
+if __name__ == '__main__':
+    parse()
+
+
+
+#%%pypdf2处理英文文本
+pdf_path = "testdata2.pdf"
+f = open(pdf_path, 'rb')
+pdf = PdfFileReader(f)
+information = pdf.getDocumentInfo()
+number_of_pages = pdf.getNumPages()
+
+page1 = pdf.getPage(28)
+a = page1.extractText()
+
+#%%
+
+def extract_information(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        pdf = PdfFileReader(f)
+        information = pdf.getDocumentInfo()
+        number_of_pages = pdf.getNumPages()
+
+    txt = f"""
+    Information about {pdf_path}: 
+
+    Author: {information.author}
+    Creator: {information.creator}
+    Producer: {information.producer}
+    Subject: {information.subject}
+    Title: {information.title}
+    Number of pages: {number_of_pages}
+    """
+
+    print(txt)
+    return information
+
+if __name__ == '__main__':
+    path = 'reportlab-sample.pdf'
+    extract_information(path)
